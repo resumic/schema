7a5b616b8e6ad124d910c2d27fb86d2f3538cee2
Suramya Shah <shah.suramya@gmail.com>
2017-10-18 22:14:02 +0530

    Modify files to create final dev markdown

--- README.md
+++
@@ -1,1 +0,0 @@
-# Resumic Schema (JSON)
--- docs/generator.go
+++ docs/generator.go
@@ -3,21 +3,20 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 	"text/template"
-
+	"docs/util"
 	"github.com/tidwall/gjson"
 )
 
 func main() {
 	var conf, doc, tmpl string
 	flag.StringVar(&conf, "conf", "schema.json", "Schema file")
-	flag.StringVar(&doc, "doc", "docs/doc.md", "Markdown file")
-	flag.StringVar(&tmpl, "tmpl", "docs/user/test.md", "Output file")
+	flag.StringVar(&doc, "doc", "docs/dev/layer.md", "Markdown file")
+	flag.StringVar(&tmpl, "tmpl", "docs/dev/doc.md", "Output file")
 	content, err := ioutil.ReadFile(conf)
 	if err != nil {
 		log.Fatal(err)
@@ -25,14 +24,14 @@ func main() {
 	f, err := os.OpenFile(doc, os.O_APPEND, 0666)
 	value := gjson.GetBytes(content, "properties")
 	value.ForEach(func(key, value gjson.Result) bool {
-		_, err = f.WriteString("\n# " + strings.Title(key.String()) + "\n" + "This section is of " + "{{.Properties." + strings.Title(key.String()) + ".Title.Description}} type that tells about the basic information of the user and consists of the following sub-sections:\n") //print name of sections
+		_, err = f.WriteString("\n# " + strings.Title(key.String()) + "\n" + "This section is of " + "{{.Properties." + strings.Title(key.String()) + ".Type}} type that tells about the basic information of the user and consists of the following sub-sections:\n") //print name of sections
 		if err != nil {
 			log.Fatal(err)
 		}
 		println(key.String())
 		result := gjson.GetBytes(content, "properties."+key.String())
 		result.ForEach(func(key1, value gjson.Result) bool {
-			_, err = f.WriteString("##    {{" + key1.String() + "}}" + "\n") //print name of sub-sections
+			_, err = f.WriteString("##    " + key1.String() + "" + "\n") //print name of sub-sections
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -42,7 +41,7 @@ func main() {
 			if key1.String() == "properties" {
 				result1 := gjson.GetBytes(content, "properties."+key.String()+"."+key1.String())
 				result1.ForEach(func(key2, value gjson.Result) bool {
-					_, err := f.WriteString("###        {{" + key2.String() + "}}" + "\nSub-section of type {{.Properties." + strings.Title(key.String()) + ".Properties." + strings.Title(key2.String()) + ".Type}},used to specify the " + key2.String() + " of the person\nThe schema snippet can be shown below:\n        " + key2.String() + ":\n        " + value.String() + "\n") //print properties of subsections
+					_, err := f.WriteString("###        " + key2.String() + "" + "\nSub-section of type {{.Properties." + strings.Title(key.String()) + ".Properties." + strings.Title(key2.String()) + ".Type}}, used to specify the " + key2.String() + " of the person\nThe schema snippet can be shown below:\n        " + key2.String() + ":\n        " + value.String() + "\n") //print properties of subsections
 					if err != nil {
 						log.Fatal(err)
 					}
@@ -54,7 +53,7 @@ func main() {
 			if key1.String() == "items" {
 				result1 := gjson.GetBytes(content, "properties."+key.String()+".items.properties")
 				result1.ForEach(func(key2, value gjson.Result) bool {
-					_, err := f.WriteString("###        {{" + key2.String() + "}}" + "\nSub-section of type {{.Properties." + strings.Title(key.String()) + ".Items.Properties." + strings.Title(key2.String()) + ".Type}},used to specify the " + key2.String() + " of the person\nThe schema snippet can be shown below:\n        " + key2.String() + ":\n        " + value.String() + "\n") //print items of subsections
+					_, err := f.WriteString("###        " + key2.String() + "" + "\nSub-section of type {{.Properties." + strings.Title(key.String()) + ".Items.Properties." + strings.Title(key2.String()) + ".Type}}, used to specify the " + key2.String() + " of the person\nThe schema snippet can be shown below:\n        " + key2.String() + ":\n        " + value.String() + "\n") //print items of subsections
 					if err != nil {
 						log.Fatal(err)
 					}
@@ -68,11 +67,10 @@ func main() {
 		return true
 	})
 	f.Close()
-	var schema SchemaData
+	var schema util.SchemaData
 	if err := json.Unmarshal(content, &schema); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(schema.Properties.Core.Type) //check
 	tpl, err := template.ParseFiles(doc)
 	if err != nil {
 		log.Fatal(err)
