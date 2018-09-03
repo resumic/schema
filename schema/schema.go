ef94ae82a0b5794c987308077abd746231d99304
Erfan Besharat <erbesharat@gmail.com>
2018-09-03 20:19:20 +0430

    Extract Location and Highlights into single structs

--- schema/schema.go
+++ schema/schema.go
@@ -6,49 +6,49 @@ import (
 	"os"
 )
 
+// Schema fields
+
 type StandardField struct {
 	Type        string `json:"type"`
 	Description string `json:"description"`
 	Format      string `json:"format,omitempty"`
 }
 
+type Location struct {
+	Type        string `json:"type"`
+	Format      string `json:"format"`
+	Description string `json:"description"`
+	Properties  struct {
+		Lat struct {
+			Type string `json:"type"`
+		} `json:"lat"`
+		Long struct {
+			Type string `json:"type"`
+		} `json:"long"`
+	} `json:"properties"`
+}
+
+type Highlights struct {
+	Type            string        `json:"type"`
+	Description     string        `json:"description"`
+	AdditionalItems bool          `json:"additionalItems"`
+	Items           StandardField `json:"items"`
+}
+
+// Main properties
 type Core struct {
 	Type                 string `json:"type"`
 	AdditionalProperties bool   `json:"additionalProperties"`
 	Properties           struct {
-		Name            StandardField `json:"name"`
-		Title           StandardField `json:"title"`
-		Image           StandardField `json:"image"`
-		Email           StandardField `json:"email"`
-		Phone           StandardField `json:"phone"`
-		URL             StandardField `json:"url"`
-		Summary         StandardField `json:"summary"`
-		CurrentLocation struct {
-			Type        string `json:"type"`
-			Format      string `json:"format"`
-			Description string `json:"description"`
-			Properties  struct {
-				Lat struct {
-					Type string `json:"type"`
-				} `json:"lat"`
-				Long struct {
-					Type string `json:"type"`
-				} `json:"long"`
-			} `json:"properties"`
-		} `json:"currentLocation"`
-		PermanentLocation struct {
-			Type        string `json:"type"`
-			Format      string `json:"format"`
-			Description string `json:"description"`
-			Properties  struct {
-				Lat struct {
-					Type string `json:"type"`
-				} `json:"lat"`
-				Long struct {
-					Type string `json:"type"`
-				} `json:"long"`
-			} `json:"properties"`
-		} `json:"permanentLocation"`
+		Name              StandardField `json:"name"`
+		Title             StandardField `json:"title"`
+		Image             StandardField `json:"image"`
+		Email             StandardField `json:"email"`
+		Phone             StandardField `json:"phone"`
+		URL               StandardField `json:"url"`
+		Summary           StandardField `json:"summary"`
+		CurrentLocation   Location      `json:"currentLocation"`
+		PermanentLocation Location      `json:"permanentLocation"`
 	} `json:"properties"`
 }
 
@@ -62,29 +62,12 @@ type Work struct {
 			Name        StandardField `json:"name"`
 			Description StandardField `json:"description"`
 			Position    StandardField `json:"position"`
-			Location    struct {
-				Type        string `json:"type"`
-				Format      string `json:"format"`
-				Description string `json:"description"`
-				Properties  struct {
-					Lat struct {
-						Type string `json:"type"`
-					} `json:"lat"`
-					Long struct {
-						Type string `json:"type"`
-					} `json:"long"`
-				} `json:"properties"`
-			} `json:"location"`
-			URL        StandardField `json:"url"`
-			StartDate  StandardField `json:"startDate"`
-			EndDate    StandardField `json:"endDate"`
-			Summary    StandardField `json:"summary"`
-			Highlights struct {
-				Type            string        `json:"type"`
-				Description     string        `json:"description"`
-				AdditionalItems bool          `json:"additionalItems"`
-				Items           StandardField `json:"items"`
-			} `json:"highlights"`
+			Location    Location      `json:"location"`
+			URL         StandardField `json:"url"`
+			StartDate   StandardField `json:"startDate"`
+			EndDate     StandardField `json:"endDate"`
+			Summary     StandardField `json:"summary"`
+			Highlights  Highlights    `json:"highlights"`
 		} `json:"properties"`
 	} `json:"items"`
 }
@@ -97,24 +80,12 @@ type Education struct {
 		AdditionalProperties bool   `json:"additionalProperties"`
 		Properties           struct {
 			Institution StandardField `json:"institution"`
-			Location    struct {
-				Type        string `json:"type"`
-				Format      string `json:"format"`
-				Description string `json:"description"`
-				Properties  struct {
-					Lat struct {
-						Type string `json:"type"`
-					} `json:"lat"`
-					Long struct {
-						Type string `json:"type"`
-					} `json:"long"`
-				} `json:"properties"`
-			} `json:"location"`
-			Area      StandardField `json:"area"`
-			StudyType StandardField `json:"studyType"`
-			StartDate StandardField `json:"startDate"`
-			EndDate   StandardField `json:"endDate"`
-			Score     struct {
+			Location    Location      `json:"location"`
+			Area        StandardField `json:"area"`
+			StudyType   StandardField `json:"studyType"`
+			StartDate   StandardField `json:"startDate"`
+			EndDate     StandardField `json:"endDate"`
+			Score       struct {
 				Type                 string `json:"type"`
 				AdditionalProperties bool   `json:"additionalProperties"`
 				Properties           struct {
@@ -147,29 +118,12 @@ type Volunteer struct {
 		Properties           struct {
 			Organization StandardField `json:"organization"`
 			Position     StandardField `json:"position"`
-			Location     struct {
-				Type        string `json:"type"`
-				Format      string `json:"format"`
-				Description string `json:"description"`
-				Properties  struct {
-					Lat struct {
-						Type string `json:"type"`
-					} `json:"lat"`
-					Long struct {
-						Type string `json:"type"`
-					} `json:"long"`
-				} `json:"properties"`
-			} `json:"location"`
-			URL        StandardField `json:"url"`
-			StartDate  StandardField `json:"startDate"`
-			EndDate    StandardField `json:"endDate"`
-			Summary    StandardField `json:"summary"`
-			Highlights struct {
-				Type            string        `json:"type"`
-				Description     string        `json:"description"`
-				AdditionalItems bool          `json:"additionalItems"`
-				Items           StandardField `json:"items"`
-			} `json:"highlights"`
+			Location     Location      `json:"location"`
+			URL          StandardField `json:"url"`
+			StartDate    StandardField `json:"startDate"`
+			EndDate      StandardField `json:"endDate"`
+			Summary      StandardField `json:"summary"`
+			Highlights   Highlights    `json:"highlights"`
 		} `json:"properties"`
 	} `json:"items"`
 }
@@ -274,28 +228,11 @@ type Projects struct {
 		Type                 string `json:"type"`
 		AdditionalProperties bool   `json:"additionalProperties"`
 		Properties           struct {
-			Name     StandardField `json:"name"`
-			Location struct {
-				Type        string `json:"type"`
-				Format      string `json:"format"`
-				Description string `json:"description"`
-				Properties  struct {
-					Lat struct {
-						Type string `json:"type"`
-					} `json:"lat"`
-					Long struct {
-						Type string `json:"type"`
-					} `json:"long"`
-				} `json:"properties"`
-			} `json:"location"`
+			Name        StandardField `json:"name"`
+			Location    Location      `json:"location"`
 			Description StandardField `json:"description"`
-			Highlights  struct {
-				Type            string        `json:"type"`
-				Description     string        `json:"description"`
-				AdditionalItems bool          `json:"additionalItems"`
-				Items           StandardField `json:"items"`
-			} `json:"highlights"`
-			Keywords struct {
+			Highlights  Highlights    `json:"highlights"`
+			Keywords    struct {
 				Type            string        `json:"type"`
 				Description     string        `json:"description"`
 				AdditionalItems bool          `json:"additionalItems"`
