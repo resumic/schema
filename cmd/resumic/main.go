df1155d96d3560773ab074c4fc06ce4de394954b
Erfan Besharat <erbesharat@gmail.com>
2018-09-05 16:49:39 +0430

    Extract schema.json generation code from case into another function

--- cmd/resumic/main.go
+++ cmd/resumic/main.go
@@ -11,11 +11,31 @@ import (
 	"github.com/resumic/schema/schema"
 )
 
+// GenerateSchema generates the schema.json file
+func GenerateSchema(schemaFile string) {
+	schema, err := schema.GetSchema()
+	if err != nil {
+		log.Fatalf("Couldn't get the schema struct: %s", err)
+	}
+	json, err := json.Marshal(schema)
+	if err != nil {
+		log.Fatal("couldn't parse the schema")
+	}
+	file, err := os.Create(schemaFile)
+	defer file.Close()
+	if err != nil {
+		log.Fatal("couldn't create the schema file")
+	}
+	_, err = file.Write(json)
+	if err != nil {
+		log.Fatal("couldn't write the schema content to given the schema file")
+	}
+	log.Printf("Schema file created successfully: %s", file.Name())
+}
+
 func main() {
-	var doc string
-	var schemaFile string
-	flag.StringVar(&doc, "doc", "../../examples/invalid/invalid_email.json", "Example file")
-	flag.StringVar(&schemaFile, "schema", "./schema.json", "Generate JSON Schema")
+	doc := flag.String("doc", "../../examples/invalid/invalid_email.json", "Example file")
+	schemaFile := flag.String("schema", "./schema.json", "Generate JSON Schema")
 	flag.Parse()
 
 	// Verify that a subcommand has been provided
@@ -25,26 +45,9 @@ func main() {
 	}
 	switch flag.Args()[0] {
 	case "validate":
-		validate.ValidateJSON(doc)
+		validate.ValidateJSON(*doc)
 	case "schema":
-		schema, err := schema.GetSchema()
-		if err != nil {
-			log.Fatalf("Couldn't get the schema struct: %s", err)
-		}
-		json, err := json.Marshal(schema)
-		if err != nil {
-			log.Fatal("couldn't parse the schema")
-		}
-		file, err := os.Create(schemaFile)
-		defer file.Close()
-		if err != nil {
-			log.Fatal("couldn't create the schema file")
-		}
-		_, err = file.Write(json)
-		if err != nil {
-			log.Fatal("couldn't write the schema content to given the schema file")
-		}
-		log.Printf("Schema file created successfully: %s", file.Name())
+		GenerateSchema(*schemaFile)
 	default:
 		log.Fatalf("Unsupported subcommands. Please check --help for commands list")
 	}
