21f68df5349ba84882c358c1b283e0cb5d80dfae
Arman Mazdaee <arman.maz1373@gmail.com>
2018-11-05 16:00:43 +0330

    Add validation using gojsonschema

--- cmd/resumic/main.go
+++ cmd/resumic/main.go
@@ -10,17 +10,44 @@ import (
 	"github.com/resumic/schema/cmd/resumic/validate"
 	"github.com/resumic/schema/jsonschema"
 	"github.com/resumic/schema/schema"
+	"github.com/xeipuuv/gojsonschema"
 )
+
+// ValidateResume validate a resume file
+func ValidateResume(resumeFile string) {
+	schema, err := jsonschema.NewSchema(schema.Schema{}, "", "Resumic Schema")
+	if err != nil {
+		log.Fatalf("couldn't get the schema struct: %s", err)
+	}
+	schemaJSON, err := json.MarshalIndent(schema, "", "  ")
+	if err != nil {
+		log.Fatal("couldn't marshal the schema")
+	}
+	schemaLoader := gojsonschema.NewBytesLoader(schemaJSON)
+	resumeLoader := gojsonschema.NewReferenceLoader(resumeFile)
+	result, err := gojsonschema.Validate(schemaLoader, resumeLoader)
+	if err != nil {
+		log.Fatalf("couldn't validate the resume: %s", err)
+	}
+	if result.Valid() {
+		log.Printf("%s is a valid resume.", resumeFile)
+	} else {
+		log.Fatalf("%s is not a valid resume", resumeFile)
+		for _, desc := range result.Errors() {
+			log.Fatal(desc)
+		}
+	}
+}
 
 // GenerateSchema generates the schema.json file
 func GenerateSchema(schemaFile string) {
 	schema, err := jsonschema.NewSchema(schema.Schema{}, "", "Resumic Schema")
 	if err != nil {
-		log.Fatalf("Couldn't get the schema struct: %s", err)
+		log.Fatalf("couldn't get the schema struct: %s", err)
 	}
 	json, err := json.MarshalIndent(schema, "", "  ")
 	if err != nil {
-		log.Fatal("couldn't parse the schema")
+		log.Fatal("couldn't marshal the schema")
 	}
 	file, err := os.Create(schemaFile)
 	defer file.Close()
@@ -34,6 +61,7 @@ func GenerateSchema(schemaFile string) {
 	log.Printf("Schema file created successfully: %s", file.Name())
 }
 
+// GenerateExample generate the example file
 func GenerateExample(exampleFile string) {
 	example := schema.NewExample()
 	json, err := json.MarshalIndent(example, "", "  ")
@@ -53,7 +81,7 @@ func GenerateExample(exampleFile string) {
 }
 
 func main() {
-	doc := flag.String("doc", "../../examples/invalid/invalid_email.json", "Example file")
+	resume := flag.String("resume", "", "Resume file")
 	schemaFile := flag.String("schema", "./schema.json", "Generate JSON Schema")
 	exampleFile := flag.String("example", "./example.json", "Generate example JSON")
 	flag.Parse()
@@ -65,7 +93,7 @@ func main() {
 	}
 	switch flag.Args()[0] {
 	case "validate":
-		validate.ValidateJSON(*doc)
+		validate.ValidateJSON(*resume)
 	case "schema":
 		GenerateSchema(*schemaFile)
 	case "example":
