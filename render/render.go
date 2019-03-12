1ac710cccd0c8bb1952ccaa1930d37e9fc90897d
Arman Mazdaee <arman.maz1373@gmail.com>
2019-03-12 15:02:55 +0330

    Make sure resume is valid before rendering

--- render/render.go
+++ render/render.go
@@ -3,9 +3,15 @@ package render
 import (
 	"io/ioutil"
 	"os"
+
+	"github.com/resumic/schema/schema"
 )
 
 func RenderHTML(resumeJSON []byte, themeDir string) ([]byte, error) {
+	if err := schema.ValidateResume(resumeJSON); err != nil {
+		return nil, err
+	}
+
 	siteDir, err := ioutil.TempDir(os.TempDir(), "resumic")
 	if err != nil {
 		return nil, err
