d109dc6a0718972e426476b16cb175f77cbfc8a6
Jahziel Villasana <jahzielv@gmail.com>
2019-04-02 11:54:19 -0400

    added a simple version command

---
+++ cmd/version.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+func check(e error) bool {
+	return e != nil
+}
+
+func versionFunc(cmd *cobra.Command, args []string) error {
+	pwd, err := os.Getwd()
+	dat, err := ioutil.ReadFile(filepath.Join(pwd, `VERSION`))
+	if check(err) {
+		return err
+	}
+	fmt.Print(string(dat))
+	return nil
+}
+
+var versionCmd *cobra.Command = &cobra.Command{
+	Use:   "version",
+	Short: "Show version information for this build of Schema",
+	Args:  cobra.ExactArgs(0),
+	RunE:  versionFunc,
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
